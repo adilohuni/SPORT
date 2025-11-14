aa55c8de2e72e2c20f58fab241b7345cdd8692ed
Lohuni <lohuniad@amazon.pl>
2025-11-14 16:36:31 +0100

    single file build

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
+  import {viteSingleFile} from 'vite-plugin-singlefile';
 
   export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), viteSingleFile()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
